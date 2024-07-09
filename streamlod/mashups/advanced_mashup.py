5c02851ed689340bdd0c4212916454098f3d26a8
lzz0722 <159738244+lzz0722@users.noreply.github.com>
2024-07-10 01:05:32 +0200

    Update advanced_mashup.py
    I tried my best for the last method.

--- streamlod/mashups/advanced_mashup.py
+++ streamlod/mashups/advanced_mashup.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Set
 import pandas as pd
 from .basic_mashup import BasicMashup
 from ..domain import Person, CulturalHeritageObject, Activity
@@ -9,45 +9,48 @@ class AdvancedMashup(BasicMashup):
     and integrates the data into unified Python objects.
     """
     
+    def __init__(self, metadata_handlers=None, process_handlers=None):
+        super().__init__()
+        if metadata_handlers is not None:
+            for handler in metadata_handlers:
+                self.addMetadataHandler(handler)
+        if process_handlers is not None:
+            for handler in process_handlers:
+                self.addProcessHandler(handler)
+
     def getActivitiesOnObjectsAuthoredBy(self, personId: str) -> List[Activity]:
-        # Get objects authored by the person
         objects = self.getCulturalHeritageObjectsAuthoredBy(personId)
-        # Get activities on these objects
         object_ids = [obj.getId() for obj in objects]
-        try:
-            df_list = [handler.getById(object_ids) for handler in self.processQuery]
-            df = pd.concat(df_list)
-            return self.toActivity(df)
-        except ValueError:
-            return list()
+        df_list = [handler.getById(object_ids) for handler in self.processQuery]
+        df = pd.concat(df_list)
+        return self.toActivity(df)
 
     def getObjectsHandledByResponsiblePerson(self, partialName: str) -> List[CulturalHeritageObject]:
-        try:
-            ids_set = set()
-            for handler in self.processQuery:
-                ids = handler.queryAttribute(attribute='refersTo', filter_condition=f"WHERE person LIKE '%{partialName}%'")
-                ids_set.update(ids)
-            return self.getCulturalHeritageObjectsByIds(list(ids_set))
-        except ValueError:
-            return list()
+        ids_set = set()
+        for handler in self.processQuery:
+            ids = handler.queryAttribute(attribute='refersTo', filter_condition=f"WHERE person LIKE '%{partialName}%'")
+            ids_set.update(ids)
+        return self.getCulturalHeritageObjectsByIds(list(ids_set))[0]
 
     def getObjectsHandledByResponsibleInstitution(self, partialName: str) -> List[CulturalHeritageObject]:
-        try:
-            ids_set = set()
-            for handler in self.processQuery:
-                ids = handler.queryAttribute(attribute='refersTo', filter_condition=f"WHERE institute LIKE '%{partialName}%'")
-                ids_set.update(ids)
-            return self.getCulturalHeritageObjectsByIds(list(ids_set))
-        except ValueError:
-            return list()
+        ids_set = set()
+        for handler in self.processQuery:
+            ids = handler.queryAttribute(attribute='refersTo', filter_condition=f"WHERE institute LIKE '%{partialName}%'")
+            ids_set.update(ids)
+        return self.getCulturalHeritageObjectsByIds(list(ids_set))[0]
 
     def getAuthorsOfObjectsAcquiredInTimeFrame(self, start: str, end: str) -> List[Person]:
-        try:
-            ids_set = set()
-            for handler in self.processQuery:
-                start_ids = set(handler.queryAttribute(attribute='refersTo', filter_condition=f"WHERE start >= '{start}'"))
-                end_ids = set(handler.queryAttribute(attribute='refersTo', filter_condition=f"WHERE end <= '{end}'"))
-                ids_set.update(start_ids.intersection(end_ids))
-            return self.getAuthorsOfCulturalHeritageObject(list(ids_set))
-        except ValueError:
-            return list()
+        ids_set = set()
+        for handler in self.processQuery:
+            ids = handler.queryAttribute(attribute='refersTo', filter_condition=f"WHERE start >= '{start}' AND end <= '{end}'")
+            ids_set.update(ids)
+
+        authors: Set[Person] = set()
+        for metadata_handler in self.metadataQuery:
+            for obj_id in ids_set:
+                author_objs_df = metadata_handler.getAuthorsOfCulturalHeritageObject([obj_id])
+                for _, row in author_objs_df.iterrows():
+                    person = Person(row['identifier'], row['name'])
+                    authors.add(person)
+
+        return list(authors)
