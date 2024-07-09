f51c3c9e33ca3050ee02cf68d41153a6b8ce09a7
lzz0722 <159738244+lzz0722@users.noreply.github.com>
2024-07-09 12:59:17 +0200

    Update advanced_mashup.py

--- streamlod/mashups/advanced_mashup.py
+++ streamlod/mashups/advanced_mashup.py
@@ -10,13 +10,37 @@ from sparql_dataframe import get
 
 class AdvancedMashup(BasicMashup): # Lin
     def getActivitiesOnObjectsAuthoredBy(self, personId: str) -> list[Activity]:
-        pass
+         # Get objects authored by the person
+        objects = self.getCulturalHeritageObjectsAuthoredBy(personId)
+        # Get activities on these objects
+        object_ids = [obj.getId() for obj in objects]
+        try:
+            df = concat(handler.getById(object_ids) for handler in self.processQuery)
+        except ValueError:
+            return list()
+        df = df[~df.index.duplicated()].sort_index(key=lambda x: x.map(key))
+        return self._to_activity(df)
 
     def getObjectsHandledByResponsiblePerson(self, partialName: str) -> list[CulturalHeritageObject]:
-        pass
+        try:
+            df = concat(handler.getActivitiesByResponsiblePerson(partialName) for handler in self.processQuery)
+        except ValueError:
+            return list()
+        df = df[~df.index.duplicated()].sort_index(key=lambda x: x.map(key))
+        return self._to_cho(df)
 
     def getObjectsHandledByResponsibleInstitution(self, partialName: str) -> list[CulturalHeritageObject]:
-        pass
+        try:
+            df = concat(handler.getActivitiesByResponsibleInstitution(partialName) for handler in self.processQuery)
+        except ValueError:
+            return list()
+        df = df[~df.index.duplicated()].sort_index(key=lambda x: x.map(key))
+        return self._to_cho(df)
 
     def getAuthorsOfObjectsAcquiredInTimeFrame(self, start: str, end: str) -> list[Person]:
-        pass
+        try:
+            df = concat(handler.getActivitiesStartedAfter(start) & handler.getActivitiesEndedBefore(end) for handler in self.processQuery)
+        except ValueError:
+            return list()
+        df = df[~df.index.duplicated()].sort_index(key=lambda x: x.map(key))
+        return self._to_person(df)
